60f1e2d80244c57f8a80b3d97ecef06fba3d62b1
Muggy Ate <muggy8@users.noreply.github.com>
2021-07-21 06:32:49 -0700

    get rid of some of the useless beginning code

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -24,6 +24,6 @@ document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    // console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    // document.getElementById('deviceready').classList.add('ready');
 }
