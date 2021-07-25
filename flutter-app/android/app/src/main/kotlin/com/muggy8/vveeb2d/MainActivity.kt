f6558c73d077a5bf0fe6125e7f62f8b340691f2e
Muggy Ate <muggy8@users.noreply.github.com>
2021-07-25 08:37:22 -0700

    doing what the dependencies needs i think

--- flutter-app/android/app/src/main/kotlin/com/muggy8/vveeb2d/MainActivity.kt
+++ flutter-app/android/app/src/main/kotlin/com/muggy8/vveeb2d/MainActivity.kt
@@ -2,5 +2,18 @@ package com.muggy8.vveeb2d
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+import `in`.jvapps.system_alert_window.SystemAlertWindowPlugin
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+
+class MainActivity: FlutterActivity(), PluginRegistrantCallback {
+
+    override fun onCreate() {
+        super.onCreate()
+        SystemAlertWindowPlugin.setPluginRegistrant(this)
+    }
+
+    override fun registerWith(registry: PluginRegistry) {
+        SystemAlertWindowPlugin.registerWith(registry.registrarFor("in.jvapps.system_alert_window"));
+    }
 }
