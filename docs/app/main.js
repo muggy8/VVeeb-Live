449c7e9db92ce91aa239fa3e10c056b449ddc2e4
Muggy Ate <muggy8@users.noreply.github.com>
2021-07-30 06:28:30 -0700

    initialize cubism framework?

--- docs/app/main.js
+++ docs/app/main.js
@@ -1,4 +1,4 @@
-(function(){
+(function(Live2D){
     let canvas = document.createElement('canvas')
 
     let gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl')
@@ -11,6 +11,12 @@
 
     document.body.appendChild(canvas);
 
-    gl.enable(gl.BLEND);
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-})()
+    gl.enable(gl.BLEND)
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+
+    let options = new Live2D.Option()
+    Live2D.CubismFramework.startUp(options)
+    Live2D.CubismFramework.initialize()
+
+
+})(window.Live2D)
