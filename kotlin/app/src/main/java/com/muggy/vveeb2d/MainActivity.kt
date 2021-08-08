0fcb77b849cb3413b7d65dd02889479cc27ed3f5
Muggy Ate <muggy8@users.noreply.github.com>
2021-08-08 12:35:15 -0700

    tried to use the mediapipe aar but it didn't work

--- kotlin/app/src/main/java/com/muggy/vveeb2d/MainActivity.kt
+++ kotlin/app/src/main/java/com/muggy/vveeb2d/MainActivity.kt
@@ -5,27 +5,96 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
+import android.graphics.SurfaceTexture
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
 import android.provider.Settings
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import android.content.res.Configuration
+import android.util.Size
+import android.view.SurfaceHolder
+import android.view.SurfaceView
 import android.view.View
-import com.google.ar.core.ArCoreApk
-import com.google.ar.core.exceptions.UnavailableException
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.google.mediapipe.components.CameraHelper.CameraFacing
+import com.google.mediapipe.components.CameraHelper.OnCameraStartedListener
+import com.google.mediapipe.components.CameraXPreviewHelper
+import com.google.mediapipe.components.ExternalTextureConverter
+import com.google.mediapipe.components.FrameProcessor
+import com.google.mediapipe.components.PermissionHelper
+import com.google.mediapipe.framework.AndroidAssetUtil
+import com.google.mediapipe.glutil.EglManager
+
 
 class MainActivity : AppCompatActivity() {
+
+    // copy-paste from mediapipe's arr example repo
+    private val TAG: String? = "MainActivity"
+
+    private val BINARY_GRAPH_NAME = "face_detection_mobile_gpu.binarypb"
+    private val INPUT_VIDEO_STREAM_NAME = "input_video"
+    private val OUTPUT_VIDEO_STREAM_NAME = "output_video"
+    private val CAMERA_FACING = CameraFacing.FRONT
+
+    // Flips the camera-preview frames vertically before sending them into FrameProcessor to be
+    // processed in a MediaPipe graph, and flips the processed frames back when they are displayed.
+    // This is needed because OpenGL represents images assuming the image origin is at the bottom-left
+    // corner, whereas MediaPipe in general assumes the image origin is at top-left.
+    private val FLIP_FRAMES_VERTICALLY = true
+
+    // {@link SurfaceTexture} where the camera-preview frames can be accessed.
+    private var previewFrameTexture: SurfaceTexture? = null
+
+    // {@link SurfaceView} that displays the camera-preview frames processed by a MediaPipe graph.
+    private var previewDisplayView: SurfaceView? = null
+
+    // Creates and manages an {@link EGLContext}.
+    private var eglManager: EglManager? = null
+
+    // Sends camera-preview frames into a MediaPipe graph for processing, and displays the processed
+    // frames onto a {@link Surface}.
+    private var processor: FrameProcessor? = null
+
+    // Converts the GL_TEXTURE_EXTERNAL_OES texture from Android camera into a regular texture to be
+    // consumed by {@link FrameProcessor} and the underlying MediaPipe graph.
+    private var converter: ExternalTextureConverter? = null
+
+    // Handles camera access via the {@link CameraX} Jetpack support library.
+    private var cameraHelper: CameraXPreviewHelper? = null
+    // copy-paste over
+
+    init {
+        System.loadLibrary("mediapipe_jni");
+        System.loadLibrary("opencv_java3");
+    }
 
     @SuppressLint("JavascriptInterface")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        previewDisplayView = SurfaceView(this)
+        setupPreviewDisplayView()
+
+        // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
+        // binary graphs.
+
+        // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
+        // binary graphs.
+        AndroidAssetUtil.initializeNativeAssetManager(this)
+
+        eglManager = EglManager(null)
+        processor = FrameProcessor(
+            this,
+            eglManager!!.getNativeContext(),
+            BINARY_GRAPH_NAME,
+            INPUT_VIDEO_STREAM_NAME,
+            OUTPUT_VIDEO_STREAM_NAME
+        )
+        processor!!.getVideoSurfaceOutput().setFlipY(FLIP_FRAMES_VERTICALLY)
+
+        PermissionHelper.checkAndRequestCameraPermissions(this)
 
 //        startOverlayButton.setOnClickListener { startOverlay() }
 //
@@ -87,7 +156,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        isARCoreSupportedAndUpToDate()
+        converter = ExternalTextureConverter(eglManager!!.context)
+        converter!!.setFlipY(FLIP_FRAMES_VERTICALLY)
+        converter!!.setConsumer(processor)
+        if (PermissionHelper.cameraPermissionsGranted(this)) {
+            startCamera()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        converter!!.close()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions!!, grantResults!!)
+        PermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     private fun getOverlayPermission(){
@@ -99,40 +183,37 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun isARCoreSupportedAndUpToDate(): Boolean {
-        return when (ArCoreApk.getInstance().checkAvailability(this)) {
-            ArCoreApk.Availability.SUPPORTED_INSTALLED -> true
-            ArCoreApk.Availability.SUPPORTED_APK_TOO_OLD, ArCoreApk.Availability.SUPPORTED_NOT_INSTALLED -> {
-                try {
-                    // Request ARCore installation or update if needed.
-                    when (ArCoreApk.getInstance().requestInstall(this, true)) {
-                        ArCoreApk.InstallStatus.INSTALL_REQUESTED -> {
-                            println("ARCore installation requested.")
-                            false
-                        }
-                        ArCoreApk.InstallStatus.INSTALLED -> true
+    private fun setupPreviewDisplayView() {
+        previewDisplayView!!.visibility = View.GONE
+        val viewGroup:ViewGroup = findViewById(R.id.preview_display_layout)
+        viewGroup.addView(previewDisplayView)
+        previewDisplayView!!
+            .getHolder()
+            .addCallback(
+                object : SurfaceHolder.Callback {
+                    override fun surfaceCreated(holder: SurfaceHolder) {
+                        processor!!.videoSurfaceOutput.setSurface(holder.surface)
                     }
-                } catch (e: UnavailableException) {
-                    println("ARCore not installed" + e.toString())
-                    false
-                }
-            }
 
-            ArCoreApk.Availability.UNSUPPORTED_DEVICE_NOT_CAPABLE ->
-                // This device is not supported for AR.
-                false
+                    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+                        // (Re-)Compute the ideal size of the camera-preview display (the area that the
+                        // camera-preview frames get rendered onto, potentially with scaling and rotation)
+                        // based on the size of the SurfaceView that contains the display.
+                        val viewSize = Size(width, height)
+                        val displaySize: Size = cameraHelper!!.computeDisplaySizeFromViewSize(viewSize)
 
-            ArCoreApk.Availability.UNKNOWN_CHECKING -> {
-                // ARCore is checking the availability with a remote query.
-                // This function should be called again after waiting 200 ms to determine the query result.
-                return ArCoreApk.getInstance().checkAvailability(this).isSupported
-            }
-            ArCoreApk.Availability.UNKNOWN_ERROR, ArCoreApk.Availability.UNKNOWN_TIMED_OUT -> {
-                // There was an error checking for AR availability. This may be due to the device being offline.
-                // Handle the error appropriately.
-                return false
-            }
-        }
+                        // Connect the converter to the camera-preview frames as its input (via
+                        // previewFrameTexture), and configure the output width and height as the computed
+                        // display size.
+                        converter!!.setSurfaceTextureAndAttachToGLContext(
+                            previewFrameTexture, displaySize.getWidth(), displaySize.getHeight()
+                        )
+                    }
+
+                    override fun surfaceDestroyed(holder: SurfaceHolder) {
+                        processor!!.videoSurfaceOutput.setSurface(null)
+                    }
+                })
     }
 
     private lateinit var overlayService: ForegroundService
@@ -154,21 +235,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (Settings.canDrawOverlays(this) && CameraPermissionHelper.hasCameraPermission(this)) {
-            startService()
-        } else {
-            Toast.makeText(this,
-                "Not all permissions are granted. Please try again.",
-                Toast.LENGTH_SHORT).show()
-        }
+    private fun startCamera() {
+        cameraHelper = CameraXPreviewHelper()
+        cameraHelper!!.setOnCameraStartedListener(
+            OnCameraStartedListener { surfaceTexture: SurfaceTexture ? ->
+                previewFrameTexture = surfaceTexture
+                // Make the display view visible to start showing the preview. This triggers the
+                // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
+                previewDisplayView!!.visibility = View.VISIBLE
+            })
+        cameraHelper!!.startCamera(this, CAMERA_FACING,  /*surfaceTexture=*/null)
     }
+
 
     private fun setViewStateToNotOverlaying(){
 //        toggleRenderer.setVisibility(View.VISIBLE)
@@ -225,11 +303,5 @@ class MainActivity : AppCompatActivity() {
 
     private fun toggleOverlayRenderer(){
         overlayService.overlay.toggleShowLive2DModel()
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        if (::overlayService.isInitialized){
-            overlayService.overlay.refreshView()
-        }
     }
 }
